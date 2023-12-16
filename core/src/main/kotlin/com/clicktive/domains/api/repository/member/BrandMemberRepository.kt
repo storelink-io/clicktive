38416513462238b9f1efbf68f5166b3d63238c60
lucas <lucas@itfluencer.co.kr>
2023-12-16 19:59:32 +0900

    fix: 오타수정

--- core/src/main/kotlin/com/clicktive/domains/api/repository/member/BrandMemberRepository.kt
+++ core/src/main/kotlin/com/clicktive/domains/api/repository/member/BrandMemberRepository.kt
@@ -1,8 +1,9 @@
 package com.clicktive.domains.api.repository.member
 
+import com.clicktive.domains.api.data.entity.member.BrandMember
 import com.clicktive.domains.api.data.entity.member.BrandMemberPk
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 @Repository
-interface BrandMemberRepository: JpaRepository<BrandMemberPk, Long> {
+interface BrandMemberRepository: JpaRepository<BrandMember, BrandMemberPk> {
 }
